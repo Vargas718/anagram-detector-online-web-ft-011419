1d12a8b2d1634ece5950d68703732f138049fb11
Rafael Vargas <vargas718@gmail.com>
2019-03-07 19:05:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+  attr_writer :match
+  def initialize(word)
+    @word = word
+    #binding.pry
+  end
+  
+  def match(anagram)
+   word.split("") == anagram.map{|words| words.split("")}
+   
+  
+  
+    binding.pry
+    
+  end
+end
